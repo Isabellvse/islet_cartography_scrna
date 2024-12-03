485c91e1fe8b88fb7ace44b51a75e38a71ab63a7
Isabellvse <isabell.ernst@hotmail.com>
2024-12-03 10:20:48 +0100

    original process

--- data_download_scripts/Scripts/Camunas/Process_Camunas.sh
+++ data_download_scripts/Scripts/Camunas/Process_Camunas.sh
@@ -8,9 +8,10 @@ conda activate /work/islet_cartography_scrna/scrna_cartography
 
 # Define variables
 study_name="Camunas"
-Genome="/work/islet_cartography_scrna/data_download_scripts/hg38/"
+Study="${study_name}.wget"
 Out="/work/scRNAseq/${study_name}/Preprocessed"
-Study="Camunas.wget"
+mkdir -p "$Out"
+Genome="/work/islet_cartography_scrna/data_download_scripts/hg38/"
 
 # Download data
 cut -f 4 $Study | xargs -n 1 -P 8 wget -q
@@ -19,7 +20,7 @@ cut -f 4 $Study | xargs -n 1 -P 8 wget -q
 awk 'NR%2==1 { print $2"\t"substr($4, 58,12) }' Camunas.wget > SRRs
 sed -i 's|_1||g' SRRs
 sed -i 's|/||g' SRRs
-awk '{ print $2"_1.fastq.gz\t"$2"_2.fastq.gz\t"$1 }' SRRs > manifest
+awk '{ print $2"_1.fq.gz\t"$2"_2.fq.gz\t"$1 }' SRRs > manifest
 
 # Run STAR
 STAR --genomeDir $Genome --soloType SmartSeq --readFilesManifest ./manifest --soloUMIdedup Exact --soloStrand Unstranded --soloFeatures Gene GeneFull --outFilterScoreMin 30 --soloMultiMappers EM --soloCellFilter None --runThreadN 20 --outMultimapperOrder Random --outSAMmultNmax 1 --readFilesCommand zcat
