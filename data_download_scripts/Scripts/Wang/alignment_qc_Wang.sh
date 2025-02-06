7353d30d47393df3b9782ad8102e89613331ca18
Isabellvse <isabell.ernst@hotmail.com>
2025-02-06 08:36:52 +0100

    alignment qc Wang

--- data_download_scripts/Scripts/Wang/alignment_qc_Wang.sh
+++ data_download_scripts/Scripts/Wang/alignment_qc_Wang.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd /work/islet_cartography_scrna/data_download_scripts/Scripts/Wang/
+
 # Activate conda environment
 # Internalize shell
 eval "$(conda shell.bash hook)"
@@ -8,6 +10,7 @@ eval "$(conda shell.bash hook)"
 conda activate /work/islet_cartography_scrna/scrna_cartography
 
 study_name="Wang"
+study="${study_name}.wget"
 data="/work/scRNAseq/${study_name}/Preprocessed"
 
 # Output folder
@@ -18,57 +21,89 @@ mkdir -p "$out"
 tmp="${data}/tmp"
 mkdir -p "$tmp"
 
-# Create CSV file for features and add prefix gene
-awk '{print $1 "_gene_feat" "," $2}' "${data}/Solo.out/Gene/Features.stats" > "$tmp/gene_features.csv"
+donors=$(cut -f 1 "$study" | sort | uniq)
 
-# Create CSV file for features and add prefix genefull
-awk '{print $1 "_genefull_feat" "," $2}' "${data}/Solo.out/GeneFull/Features.stats" > "$tmp/genefull_features.csv"
+for z in $donors; do
+    # Create CSV file for features and add prefix gene
+    awk '{print $1 "_gene_feat" "," $2}' "${data}/${z}/Solo.out/Gene/Features.stats" > "$tmp/gene_features.csv"
 
-# Add prefix to summary CSV file gene
-awk -F',' '{print $1 "_gene_sum" "," $2}' "${data}/Solo.out/Gene/Summary.csv" > "$tmp/gene_summary.csv"
+    # Create CSV file for features and add prefix genefull
+    awk '{print $1 "_genefull_feat" "," $2}' "${data}/${z}/Solo.out/GeneFull/Features.stats" > "$tmp/genefull_features.csv"
 
-# Add prefix to summary CSV file genefull
-awk -F',' '{print $1 "_genefull_sum" "," $2}' "${data}/Solo.out/GeneFull/Summary.csv" > "$tmp/genefull_summary.csv"
+    # Add prefix to summary CSV file gene
+    awk -F',' '{print $1 "_gene_sum" "," $2}' "${data}/${z}/Solo.out/Gene/Summary.csv" > "$tmp/gene_summary.csv"
 
-# Create CSV file for barcode.stats
-awk '{print $1 "_barcode" "," $2}' "${data}/Solo.out/Barcodes.stats" > "$tmp/barcode.csv"
+    # Add prefix to summary CSV file genefull
+    awk -F',' '{print $1 "_genefull_sum" "," $2}' "${data}/${z}/Solo.out/GeneFull/Summary.csv" > "$tmp/genefull_summary.csv"
 
-# Process log file 
-# divide file by | and remove first 4 rows
-awk -F'|' 'NR > 4 {
+    # Create CSV file for barcode.stats
+    awk '{print $1 "_barcode" "," $2}' "${data}/${z}/Solo.out/Barcodes.stats" > "$tmp/barcode.csv"
+
+    # Process log file 
+    # divide file by | and remove first 4 rows
+    awk -F'|' 'NR > 4 {
     # Divide by |
-    gsub(/^ +| +$/, "", $1);
-    gsub(/^ +| +$/, "", $2);
+        gsub(/^ +| +$/, "", $1);
+        gsub(/^ +| +$/, "", $2);
     # replace white space with _    
-    gsub(/ /, "_", $1);
-    gsub(/ /, "_", $2);
+        gsub(/ /, "_", $1);
+        gsub(/ /, "_", $2);
     # replace , with _
-    gsub(/,/, "_", $1);
-    gsub(/,/, "_", $2);
+        gsub(/,/, "_", $1);
+        gsub(/,/, "_", $2);
     # remove :
-    gsub(/:/, "", $1);
-    if ($1 != "" && $2 != "") {
-        print $1 "," $2
-    }
-}' "${data}/Log.final.out" > "$tmp/log.csv"
+        gsub(/:/, "", $1);
+        if ($1 != "" && $2 != "") {
+            print $1 "," $2
+        }
+    }' "${data}/${z}/Log.final.out" > "$tmp/log.csv"
 
-# Create combined file
-echo "sample, $study_name" > "$tmp/combined.csv"
+    # Create combined file
+    echo "sample, $z" > "$tmp/${z}_combined.csv"
 
-# Set executable permission for the CSV files
-chmod +x "$tmp"/*.csv
+    # Set executable permission for the CSV files
+    chmod +x "$tmp"/*.csv
 
-# Combine the files
-cat "$tmp/log.csv" "$tmp/barcode.csv" "$tmp/gene_features.csv" "$tmp/gene_summary.csv" "$tmp/genefull_features.csv" "$tmp/genefull_summary.csv" >> "$tmp/combined.csv"
+    # Combine the files
+    cat "$tmp/log.csv" "$tmp/barcode.csv" "$tmp/gene_features.csv" "$tmp/gene_summary.csv" "$tmp/genefull_features.csv" "$tmp/genefull_summary.csv" >> "$tmp/${z}_combined.csv"
 
-# Clean up intermediate files
-rm "$tmp/log.csv" "$tmp/barcode.csv" "$tmp/gene_features.csv" "$tmp/gene_summary.csv" "$tmp/genefull_features.csv" "$tmp/genefull_summary.csv"
+    # Clean up intermediate files for this donor
+    rm "$tmp/log.csv" "$tmp/barcode.csv" "$tmp/gene_features.csv" "$tmp/gene_summary.csv" "$tmp/genefull_features.csv" "$tmp/genefull_summary.csv"
+done
 
-# Sort the combined file
-sort "$tmp/combined.csv" -o "$tmp/combined_sorted.csv"
+# Combine all files into one
+files=($tmp/*combined.csv)
+
+# Extract the header from the first file
+head -n 1 "${files[0]}" > "$tmp/header0.csv"
+
+# Extract headers from all files and combine them
+for ((i = 1; i < ${#files[@]}; i++)); do
+    head -n 1 "${files[i]}" | cut -d, -f2- > "$tmp/header${i}.csv"
+done
+
+# Combine headers into a single line
+paste -d, "$tmp/header0.csv" $(for ((i = 1; i < ${#files[@]}; i++)); do echo "$tmp/header${i}.csv"; done) > "$tmp/combined_header.csv"
+
+# Remove headers from the original files and sort them
+for file in "${files[@]}"; do
+    tail -n +2 "$file" | sort > "${file}_sorted"
+done
+
+# Initialize the combined body file with the first sorted file
+cp "${files[0]}_sorted" "$tmp/combined_body.csv"
+
+# Loop through the rest of the sorted files and join them
+for ((i = 1; i < ${#files[@]}; i++)); do
+    join -t, -1 1 -2 1 "$tmp/combined_body.csv" "${files[i]}_sorted" > "$tmp/temp_combined.csv"
+    mv "$tmp/temp_combined.csv" "$tmp/combined_body.csv"
+done
+
+# Combine the header and body
+cat "$tmp/combined_header.csv" "$tmp/combined_body.csv" > "$tmp/combined_t.csv"
 
 # Transpose
-datamash -t, transpose < "$tmp/combined_sorted.csv" > "$out/alignment_qc_${study_name}.csv"
+datamash -t, transpose < "$tmp/combined_t.csv" > "$out/alignment_qc_${study_name}.csv"
 
 # Clean up
 rm -r "$tmp"
