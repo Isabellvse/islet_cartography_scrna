2a91bbd1ca090db3560bae34f0d757a515cc01cf
Isabellvse <isabell.ernst@hotmail.com>
2025-06-24 08:08:17 +0200

    Added functions to create ann data objects

--- scripts/misc/my_anndata.py
+++ scripts/misc/my_anndata.py
@@ -4,6 +4,13 @@ import os
 from pathlib import Path
 from collections import defaultdict
 
+def vprint(msg, verbose = True):
+    """
+    Helper function for verbose
+    """
+    if verbose:
+        print(msg)
+        
 def create_directories(dir_path, verbose):
     """
     Create a directory if it does not already excist
@@ -25,7 +32,7 @@ def create_directories(dir_path, verbose):
 
 # Define if data used to load raw counts should come from Gene or
 # GeneFull_Ex50pAS path
-def define_file_path_from_meta(metadata_df):
+def define_file_path_from_meta(metadata_df, verbose = False):
     """
     Define if raw counts should come from the Gene or GeneFull_Ex50pAS path
     
@@ -43,18 +50,17 @@ def define_file_path_from_meta(metadata_df):
 
     if cell_nuclei == "cell":
         path = "Gene"
-        print(f"[DIR] Path is: ", path)
+        vprint(f"[DIR] Path is: {path}", verbose)
         return path
 
     elif cell_nuclei == "nuclei":
         path = "GeneFull_Ex50pAS"
-        print(f"[DIR] Subfolder is: ", path)
+        vprint(f"[DIR] Subfolder is: {path}", verbose)
         return path
 
     else:
         print("[X] No match of cell or nuclei!")
         return None
-
 
 def create_anndata_per_sample(sample_id, base_data_dir, metadata_path,
     matrix_filename = "matrix.mtx", features_filename = "features.tsv", barcodes_filename = "barcodes_prefixed.tsv", 
@@ -77,178 +83,144 @@ def create_anndata_per_sample(sample_id, base_data_dir, metadata_path,
         An AnnData object from the scanpy package containing expression data and metadata,
         or None if the processing failed or data is missing.  
     """
-    if verbose:
-        print(f" ============= Processing sample: {sample_id} =============")
 
-        # Get subfolder path from meta data
-        meta = pd.read_csv(
-            f"{metadata_path}",
-            sep=",",
-            index_col=metadata_barcode_col)
+    vprint(f" ============= Processing sample: {sample_id} =============", verbose)
 
+    # Get subfolder path from meta data
+    meta = pd.read_csv(
+        f"{metadata_path}",
+        sep=",",
+        index_col=metadata_barcode_col)
+
+    vprint(
+        f"[OK] 'Meta data' loaded successfully with shape {
+            meta.shape} (rows, columns)", verbose)
+    vprint(meta.iloc[0:2, 0:3], verbose)
+
+    # subset meta data to only contain sample_id
+    meta_sub = meta[meta["sample"] == sample_id]
+
+    # Define subfolder from metadata
+    subfolder = define_file_path_from_meta(metadata_df=meta_sub)
+
+    if subfolder is None:
         print(
-            f"[OK] 'Meta data' loaded successfully with shape {
-                meta.shape} (rows, columns)")
-        print(meta.iloc[0:2, 0:3])
+            f"[X] No valid subfolder path found for sample {sample_id}. Check your meta data or sample id! ... Skipping.")
+        return None  # Early exit
 
-        # subset meta data to only contain sample_id
-        meta_sub = meta[meta["sample"] == sample_id]
+    # Build path to raw data
+    sample_raw_data_path = os.path.join(
+        base_data_dir, sample_id, "Solo.out", subfolder, "raw"
+    )
 
-        # Define subfolder from metadata
-        subfolder = define_file_path_from_meta(metadata_df=meta_sub)
+    vprint(f"[DIR] Path to raw data: {sample_raw_data_path}", verbose)
 
-        if subfolder is None:
-            if verbose:
-                print(
-                    f"[X] No valid subfolder path found for sample {sample_id}. Check your meta data or sample id! ... Skipping.")
-                return None  # Early exit
-
-        # Build path to raw data
-        sample_raw_data_path = os.path.join(
-            base_data_dir, sample_id, "Solo.out", subfolder, "raw"
-        )
-
-        if verbose:
-            print(f"[DIR] Path to raw data: {sample_raw_data_path}")
-
-            # Check if data path exists for the sample
-            if not os.path.exists(sample_raw_data_path):
-
-                if verbose:
-                    print(
-                        f"[X] Data path not found for {sample_id}: {sample_raw_data_path}. Skipping this sample.")
-                    return None
+    # Check if data path exists for the sample
+    if not os.path.exists(sample_raw_data_path):
+        print(
+            f"[X] Data path not found for {sample_id}: {sample_raw_data_path}. Skipping this sample.")
+        return None
 
     # Construct paths for matrix, features, and barcodes files
     mtx_file = os.path.join(sample_raw_data_path, matrix_filename)
-    print(f"[DIR] MTX file path is: ", mtx_file)
     features_file = os.path.join(sample_raw_data_path, features_filename)
-    print(f"[DIR] Feature file path is: ", features_file)
     barcodes_file = os.path.join(sample_raw_data_path, barcodes_filename)
-    print(f"[DIR] Barcode file path is: ", barcodes_file)
 
     # Load barcodes
     bar = pd.read_csv(barcodes_file, sep="\t", index_col=0, header=None)
 
-    if verbose:
-        print(
-            f"[OK] 'barcodes' loaded for {sample_id} with shape {
-                bar.shape}. Preview:\n{
-                bar.head()}")
+    vprint(f"[OK] 'barcodes' loaded for {sample_id} with shape {
+            bar.shape}. Preview:\n{
+            bar.head()}", verbose)
 
     # Read expression matrix
-    if verbose:
-        print("[..] Reading expression matrix from .mtx file...")
+    vprint("[..] Reading expression matrix from .mtx file...", verbose)
     X_tmp = sc.read_mtx(mtx_file)
-    if verbose:
-        print(
-            f"[OK] Raw matrix loaded with shape (genes, cells): {
-                X_tmp.X.shape}")
+    vprint(f"[OK] Raw matrix loaded with shape (genes, cells): {X_tmp.X.shape}", verbose)
 
     # Transpose matrix
-    if verbose:
-        print("[TRA] Transposing matrix to match AnnData format (cells × genes)...")
+    vprint("[TRA] Transposing matrix to match AnnData format (cells × genes)...", verbose)
     X_matrix = X_tmp.X.T
-    if verbose:
-        print(
-            f"[OK] Matrix transposed. New shape (cells, genes): {
-                X_matrix.shape}")
+    vprint(f"[OK] Matrix transposed. New shape (cells, genes): {X_matrix.shape}", verbose)
 
     # Load features (genes)
     var_df = pd.read_csv(features_file, sep="\t", index_col=0, header=None)
     var_df.columns = ["gene_name", "feature_type"]
 
-    if verbose:
-        print(
-            f"[OK] 'features' (genes) loaded with shape {
-                var_df.shape}. Preview:\n{
-                var_df.head()}")
+    vprint(f"[OK] 'features' (genes) loaded with shape {var_df.shape}. Preview:\n{var_df.head()}", verbose)
 
     # Create AnnData object
-    if verbose:
-        print("[..] Creating AnnData object...")
+    vprint("[..] Creating AnnData object...", verbose)
 
     X = sc.AnnData(X_matrix, obs=bar, var=var_df)
 
-    if verbose:
-        print(f"[OK] AnnData created with shape: {X.shape} (cells × genes)")
+    vprint(f"[OK] AnnData created with shape: {X.shape} (cells × genes)", verbose)
 
     # Subset meta data object to only contain barcodes found in the 'barcodes'
     # file..."
-    if verbose:
-        print("[SUB] Subsetting metadata to barcodes found in the current sample...")
+    vprint("[SUB] Subsetting metadata to barcodes found in the current sample...", verbose)
 
     meta_bar_subset = meta.loc[[
         x for x in bar.index.tolist() if x in meta.index]]
 
     if meta_bar_subset.empty:
-        if verbose:
-            print(
-                "[!!] Subsetting resulted in an empty metadata DataFrame for this sample. This might indicate no matching barcodes. Skipping."
-            )
+        print("[!!] Subsetting resulted in an empty metadata DataFrame for this sample. This might indicate no matching barcodes. Skipping.")
         return None
 
-    if verbose:
-        print(f"[OK] Metadata subsetted to {meta_bar_subset.shape} cells.")
-        print("[~] Preview of subsetted metadata:")
-        print(meta_bar_subset.iloc[0:2, 0:3])
+    vprint(f"[OK] Metadata subsetted to {meta_bar_subset.shape} cells.", verbose)
 
     # Subset AnnData object to only contain barcodes found in the subsetted
     # metadata
-    if verbose:
-        print("[SUB] Subsetting AnnData object to cells with matching metadata...")
+    vprint("[SUB] Subsetting AnnData object to cells with matching metadata...", verbose)
 
-        X_sub = X[meta_bar_subset.index].copy()
+    X_sub = X[meta_bar_subset.index].copy()
 
-    if verbose:
-        print(
-            f"[OK] AnnData has been subset to shape: {
-                X_sub.shape} (cells × genes)")
+    vprint(
+        f"[OK] AnnData has been subset to shape: {
+            X_sub.shape} (cells × genes)", verbose)
 
     # Align meta_bar_subset to X_sub.obs's index order
     meta_bar_subset = meta_bar_subset.loc[X_sub.obs.index]
 
-    if verbose:
-        assert set(meta_bar_subset.index).issubset(
-            set(X_sub.obs.index)
-        ), "Some barcodes in obs_bar are not in X_sub.obs"
-        print("[OK] Barcodes in meta data are found in AnnData.")
+    assert set(meta_bar_subset.index).issubset(
+        set(X_sub.obs.index)
+    ), "Some barcodes in obs_bar are not in X_sub.obs"
+    vprint("[OK] Barcodes in meta data are found in AnnData.", verbose)
 
-    if verbose:
-        # Print which barcodes are missing
-        missing = set(meta_bar_subset.index) - set(X.obs.index)
+    # Print which barcodes are missing
+    missing = set(meta_bar_subset.index) - set(X.obs.index)
 
-        if missing:
-            print(f"Missing barcodes in X.obs: {missing}")
-            return None
-        else:
-            print("[OK] No missing barcodes in meta data.")
+    if missing:
+        print(f"Missing barcodes in X.obs: {missing}")
+        return None
+    else:
+        vprint("[OK] No missing barcodes in meta data.", verbose)
 
-            # Align obs_bar to X_sub.obs's index order
-            meta_bar_subset = meta_bar_subset.loc[X_sub.obs.index]
+        # Align obs_bar to X_sub.obs's index order
+        meta_bar_subset = meta_bar_subset.loc[X_sub.obs.index]
 
-            # Check if the indices are now aligned and in the same order
-            assert meta_bar_subset.index.equals(
-                X_sub.obs.index
-            ), "Indices are not in the same order."
-            print("[OK] Indices in meta data are in the same order as the AnnData.")
+        # Check if the indices are now aligned and in the same order
+        assert meta_bar_subset.index.equals(
+            X_sub.obs.index
+        ), "Indices are not in the same order."
+        vprint("[OK] Indices in meta data are in the same order as the AnnData.", verbose)
 
-            print("[..] Adding metadata columns from meta data to AnnData...")
+        vprint("[..] Adding metadata columns from meta data to AnnData...", verbose)
 
-            # Ensure obs_bar is aligned to X_sub.obs
-            meta_bar_subset = meta_bar_subset.loc[X_sub.obs.index]
+        # Ensure obs_bar is aligned to X_sub.obs
+        meta_bar_subset = meta_bar_subset.loc[X_sub.obs.index]
 
-            for col in meta_bar_subset.columns:
-                X_sub.obs[col] = meta_bar_subset[col]
+        for col in meta_bar_subset.columns:
+            X_sub.obs[col] = meta_bar_subset[col]
+            vprint(f"[OK] Added column: '{col}'", verbose)
 
-                print(f"[OK] Added column: '{col}'")
+        # Use assert to confirm metadata was copied exactly
+        assert X_sub.obs[meta_bar_subset.columns].equals(
+            meta_bar_subset
+        ), "[X] Metadata was not added correctly to AnnData!"
 
-            # Use assert to confirm metadata was copied exactly
-            assert X_sub.obs[meta_bar_subset.columns].equals(
-                meta_bar_subset
-            ), "[X] Metadata was not added correctly to AnnData!"
+        vprint("[OK] All metadata columns added successfully and verified!", verbose)
 
-            print("[OK] All metadata columns added successfully and verified!")
     return X_sub
 
 
