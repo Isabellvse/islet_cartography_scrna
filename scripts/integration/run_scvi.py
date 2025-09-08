8f4644dcb8be9cba24063a6fc79393195d41b087
Isabellvse <isabell.ernst@hotmail.com>
2025-09-08 08:26:45 +0200

    scripts for integration test

---
+++ scripts/integration/run_scvi.py
@@ -0,0 +1,69 @@
+# run_scvi.py
+import os
+import sys
+import scanpy as sc
+import scvi
+import pandas as pd
+import pickle
+
+scvi.settings.seed = 0
+scvi.settings.dl_num_workers = 62
+scvi.settings.dl_persistent_workers = True  # keeps workers alive between epochs
+print("Last run with scvi-tools version:", scvi.__version__)
+
+n = int(sys.argv[1])
+adata_file = sys.argv[2]
+model_dir  = sys.argv[3]
+file_dir   = sys.argv[4]
+key_batch0 = sys.argv[5]
+key_batch1 = sys.argv[6]
+key_save   = sys.argv[7]
+latent_dims = int(sys.argv[8])
+embedding_dims = int(sys.argv[9])
+
+adata = sc.read_h5ad(adata_file)
+
+# Setup scVI
+scvi.model.SCVI.setup_anndata(
+    adata,
+    layer='counts',
+    batch_key=key_batch0,
+    categorical_covariate_keys=[key_batch1]
+)
+
+model = scvi.model.SCVI(
+    adata,
+    n_latent=latent_dims,
+    batch_representation="embedding",
+    batch_embedding_kwargs={'embedding_dim': embedding_dims}
+)
+model.train()
+
+# Save model (optional)
+model_path = os.path.join(model_dir, f'scvi_model_{n}_{key_save}')
+model.save(model_path, overwrite=True)
+
+# Save latent embedding
+latent = model.get_latent_representation()
+latent_df = pd.DataFrame(latent, index=adata.obs_names,
+                         columns=[f"scvi_{i}" for i in range(latent.shape[1])])
+df_path = os.path.join(file_dir, f"scvi_latent_embd_{n}_{key_save}.csv")
+latent_df.to_csv(df_path)
+
+# Save model to disk
+model_path = os.path.join(model_dir, f'scvi_model_{n}_{key_save}')
+model.save(model_path, overwrite=True)
+
+# Save model entires add to adata object
+scvi_meta = {
+"_scvi_uuid": adata.uns.get("_scvi_uuid", None),
+"_scvi_manager_uuid": adata.uns.get("_scvi_manager_uuid", None)}
+
+uuid_path=os.path.join(file_dir, f'scvi_{n}_{key_save}_model_uuid.pkl')
+
+with open(uuid_path, "wb") as f:
+    pickle.dump(scvi_meta, f)
+
+del adata
+import gc
+gc.collect()
