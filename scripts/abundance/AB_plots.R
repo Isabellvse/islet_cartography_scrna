ebb5d97daeb1661e4253671035f07f1a1586b711
“Isabellvse” <isabell.ernst@hotmail.com>
2026-04-24 20:42:23 +0200

    testing

--- scripts/abundance/AB_plots.R
+++ scripts/abundance/AB_plots.R
@@ -8,6 +8,7 @@ set.seed(1000)
 # Load_data ---------------------------------------------------------------
 t2d <- vroom::vroom(here::here("islet_cartography_scrna/data/milo/files/t2d_vs_nd.csv"))
 pre <- vroom::vroom(here::here("islet_cartography_scrna/data/milo/files/pre_vs_nd.csv"))
+df <- vroom::vroom(here::here("islet_cartography_scrna/data/milo/de_analysis_overall/t2d_for_de_overall.csv"))
 
 
 # Boxplot -----------------------------------------------------------------
@@ -108,3 +109,78 @@ pdf(
 )
 (ppre + pt2d) + plot_layout(guides = 'collect')
 dev.off()
+
+
+# Number of cells in each neighborhood ------------------------------------
+table <- df |> 
+  dplyr::group_by(nhood_id) |> 
+  dplyr::summarise(n_cells = dplyr::n()) |> 
+  dplyr::summarise(median = round(median(n_cells), 2),
+                   mean = round(mean(n_cells), 2),
+                   min = round(min(n_cells), 2),
+                   max = round(max(n_cells), 2))
+pcell <- df |> 
+  dplyr::group_by(nhood_id) |> 
+  dplyr::summarise(n_cells = dplyr::n()) |> 
+  ggplot2::ggplot(ggplot2::aes(x = n_cells)) +
+  ggplot2::geom_histogram() +
+  ggplot2::annotate(geom = 'table',
+                    x = 7000, 
+                    y = 100,
+                    label=list(table),
+                    table.theme = gridExtra::ttheme_minimal(
+                      base_size = 4)) +
+  ggplot2::labs(title = "Number of cells",
+                x = "Total number of cells\nper neighborhood",
+                y = "Count") +
+  my_theme()
+
+
+# Number of samples -------------------------------------------------------
+table <- df |> 
+  dplyr::select(nhood_id, ic_id_platform_adjusted_sample) |> 
+  dplyr::distinct() |> 
+  dplyr::group_by(nhood_id) |> 
+  dplyr::summarise(n_samples = dplyr::n()) |> 
+  dplyr::summarise(median = round(median(n_samples), 2),
+                   mean = round(mean(n_samples), 2),
+                   min = round(min(n_samples), 2),
+                   max = round(max(n_samples), 2))
+
+
+psample <- df |> 
+  dplyr::select(nhood_id, ic_id_platform_adjusted_sample) |> 
+  dplyr::distinct() |> 
+  dplyr::group_by(nhood_id) |> 
+  dplyr::summarise(n_samples = dplyr::n()) |> 
+  ggplot2::ggplot(ggplot2::aes(x = n_samples)) +
+  ggplot2::geom_histogram() +
+  ggplot2::labs(title = "Number of samples",
+                x = "Total number of samples\nper neighborhood",
+                y = "Count") +
+  my_theme()
+
+# Purity ------------------------------------------------------------------
+table <- df |> 
+  dplyr::select(nhood_id, nhood_annotation_frac) |> 
+  dplyr::distinct() |> 
+  dplyr::summarise(median = round(median(nhood_annotation_frac), 3),
+                   mean = round(mean(nhood_annotation_frac), 3),
+                   min = round(min(nhood_annotation_frac), 3),
+                   max = round(max(nhood_annotation_frac), 3))
+
+ppure <- df |> 
+  dplyr::select(nhood_id, nhood_annotation_frac) |> 
+  dplyr::distinct() |> 
+  ggplot2::ggplot(ggplot2::aes(x = nhood_annotation_frac)) +
+  ggplot2::geom_histogram(bins = 100) +
+  ggplot2::annotate(geom = 'table',
+                    x = 0.2, 
+                    y = 300,
+                    label=list(table),
+                    table.theme = gridExtra::ttheme_minimal(
+                      base_size = 4)) +
+  ggplot2::labs(title = "Purity",
+                x = "Fraction of cell types\nin neighborhood",
+                y = "Count") +
+  my_theme()
