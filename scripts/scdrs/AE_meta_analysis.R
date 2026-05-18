7589b81846c26ec563f0f44e8925f0cc1ad802f3
Isabellvse <isabell.ernst@hotmail.com>
2026-05-18 10:38:19 +0200

    meta data analysis of scdrs results

---
+++ scripts/scdrs/AE_meta_analysis.R
@@ -0,0 +1,7 @@
+# Description -------------------------------------------------------------
+# Perform meta analysis of disease relevance scores (reference: https://doing-meta.guide/intro#history)
+
+# Set up ------------------------------------------------------------------
+base::source(here::here("islet_cartography_scrna/scripts/misc/set_up.R"))
+set.seed(1000)
+
