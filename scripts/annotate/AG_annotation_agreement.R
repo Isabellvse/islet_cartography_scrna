c4def5e87bad246b9e583627f50b506e9a443c88
Isabellvse <isabell.ernst@hotmail.com>
2026-09-07 12:34:33 +0200

    plotting annotaiton agreement cell type broad and cell type

--- scripts/annotate/AG_annotation_agreement.R
+++ scripts/annotate/AG_annotation_agreement.R
@@ -13,17 +13,8 @@ iridescent <- khroma::color("iridescent")
 meta <- vroom::vroom(here::here("islet_cartography_scrna/data/annotate/files/obs.csv")) 
 
 
-# Overlap with harmonized labels ------------------------------------------
-# Broad cell type annotaiton
-meta |> 
-  dplyr::mutate(agreement = as.numeric(study_cell_annotation_harmonized == cell_type_broad)) |> 
-  dplyr::group_by(cell_type_broad) |>
-  dplyr::summarise(total = dplyr::n(),
-                   consistent = sum(agreement),
-                   inconsistent = total-consistent,
-                   pct_con = consistent / total,
-                   pct_incon = inconsistent / total)
 
+# Broad annotation --------------------------------------------------------
 confusion <- meta |>
   dplyr::count(cell_type_broad, 
                study_cell_annotation_harmonized, 
@@ -39,7 +30,7 @@ confusion <- meta |>
 
 # of the cells I labeled X, what fraction were called Y by the study
 pdf(
-  file = paste0(here::here("islet_cartography_scrna/data/annotate/plot/annotation_agreement.pdf")),
+  file = paste0(here::here("islet_cartography_scrna/data/annotate/plot/annotation_agreement_cell_type_broad.pdf")),
   height = 2,
   width = 3)
 confusion |>
@@ -50,6 +41,41 @@ confusion |>
     x = "Study annotation",
     y = "Cell type broad",
     fill = "Fraction of\n cell type broad"
+  ) +
+  ggplot2::scale_x_discrete(labels = function(x) stringr::str_to_title(x)) +
+  ggplot2::scale_y_discrete(labels = function(x) stringr::str_to_title(x)) +
+  my_theme() +
+  ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, hjust = 1, vjust = 0.5))
+dev.off()
+
+
+# cell type ---------------------------------------------------------------
+confusion <- meta |>
+  dplyr::count(cell_type, 
+               study_cell_annotation_harmonized, 
+               name = "n") |>
+  tidyr::complete(
+    cell_type,
+    study_cell_annotation_harmonized,
+    fill = list(n = 0)
+  ) |> 
+  dplyr::group_by(cell_type) |>
+  dplyr::mutate(pct = n / sum(n)) |>
+  dplyr::ungroup() 
+
+# of the cells I labeled X, what fraction were called Y by the study
+pdf(
+  file = paste0(here::here("islet_cartography_scrna/data/annotate/plot/annotation_agreement_cell_type.pdf")),
+  height = 2,
+  width = 3)
+confusion |>
+  ggplot2::ggplot(ggplot2::aes(x = study_cell_annotation_harmonized, y = cell_type, fill = pct)) +
+  ggplot2::geom_tile(color = "white") +
+  ggplot2::scale_fill_gradientn(colors = iridescent(5), limits=c(0,1)) +
+  ggplot2::labs(
+    x = "Study annotation",
+    y = "Cell type",
+    fill = "Fraction of\n cell type"
   ) +
   ggplot2::scale_x_discrete(labels = function(x) stringr::str_to_title(x)) +
   ggplot2::scale_y_discrete(labels = function(x) stringr::str_to_title(x)) +
